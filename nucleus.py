ba8caed8f4efadd8a062353f4b93da827a328574
oneling0517 <63098487+oneling0517@users.noreply.github.com>
2021-12-16 22:19:13 +0800

    Update nucleus.py

--- nucleus.py
+++ nucleus.py
@@ -263,8 +263,8 @@ def train(model, dataset_dir, subset):
     # Image augmentation
     # http://imgaug.readthedocs.io/en/latest/source/augmenters.html
     augmentation = iaa.SomeOf((0, 2), [
-        #iaa.Fliplr(0.5),
-        #iaa.Flipud(0.5),
+        iaa.Fliplr(0.5),
+        iaa.Flipud(0.5),
         iaa.OneOf([iaa.Affine(rotate=90),
                    iaa.Affine(rotate=180),
                    iaa.Affine(rotate=270)]),
